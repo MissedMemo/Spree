9c998bfdcd9c8a29972597e1bcdff6d5b2730871
Flattened Threat <flattenedthreat@Shantis-MacBook-Air.local>
2016-04-02 16:02:47 -0700

    started floating button

--- mobile/src/components/map/map.js
+++ mobile/src/components/map/map.js
@@ -2,110 +2,56 @@ var React = require('react-native');
 var Icon_Restaurant = require('../images/icon_restaurant.png');
 var Icon_Shopping = require('../images/icon_shopping.png');
 var Icon_Pub = require('../images/icon_pub.png');
+var Button = require('../common/button');
 
 var {
   Component,
   StyleSheet,
   MapView,
+  TouchableHighlight,
   View,
   Text
 } = React;
-
-var Button = require('../common/button');
 
 module.exports = React.createClass({
 
   getInitialState: function() {
     return {
       markers: [], // for pins
-      activityRootUrl: 'http://adventureus.herokuapp.com/api/activities' // jenna: needs to be prop, not sure where to put it
     };
   },
 
   componentWillMount: function(){
-
-    // holder array for pins
-    var tempMarker = [];
-
-    //get activities
-    return fetch( this.state.activityRootUrl )
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(json){
-
-        for (var i = 0; i < json.length; i++) {
-
-          // holder for region object
-          var holder = json[i].region;
-
-          // add title, description, image to region object
-          holder['title'] = json[i].title;
-          holder['subtitle'] = json[i].description;
-
-          console.log('json[i]: ', json[i]);
-
-          // assign pin image based on category
-          if ( json[i].category === 'Restaurant' ) {
-            holder['image'] = Icon_Restaurant;
-            console.log('inside restaurant! ');
-          }
-          else if ( json[i].category === 'Bar' ) {
-            holder['image'] = Icon_Pub;
-            console.log('inside bar! ');
-          }
-          else if ( json[i].category === 'Shopping' ) {
-            holder['image'] = Icon_Shopping;
-            console.log('inside shopping! ');
-          }
-
-          tempMarker.push(holder);
-        }
-
-        // set pins state
-        this.setState({
-          markers: tempMarker
-        });
-
-      }.bind(this));
-
-      console.log('this.state.markers: ', this.state.markers);
   },
 
   render: function(){
 
     return(
 
-        <View style={styles.container}>
-          <MapView 
-          annotations={this.state.markers}
+      <View style={ styles.container }>
+      
+        <MapView
+          style={styles.map}
+          annotations={ this.state.mapPins }
           showsUserLocation={true}
           followUserLocation={true}
-          style={styles.map}
-          >
-          </MapView>
-
-          <Button style={styles.button} text={'Add Activity'} onPress={this.onPress} />
-          
+        />
+        <View style={ styles.floatingButton } >
+          <Text style={ styles.buttonText } >Add</Text>
         </View>
+      </View>
     );
   },
 
-  //when the user stops dragging the map this function is called
-  onRegionChangeComplete: function(region){
-    console.log(region);
-  },
 
-  //refer to this on line 20.
-  onPress: function() {
-    //navigate over to signup
-    //push into the navigator stack
-    this.props.navigator.push({name: 'activities'})
+  showActivity: function() {
+    this.props.navigator.push( {name: 'activities'} )
   }
 
 })
 
 var styles = StyleSheet.create ({
+
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -113,13 +59,23 @@ var styles = StyleSheet.create ({
   },
 
   map: {
-    flex: 2,
-    marginTop: 30
+    flex: 1,
+    marginTop: 30 // offset for wifti, time, battery etc. display
   },
 
-  buttonWrapper: {
-   flex: 1,
-   alignItems: 'center'
+  floatingButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#6e73ee'
+  },
+
+  buttonText: {
+    flex: 1,
+    alignSelf: 'center',
   }
 
 });
