c326a7ab5663670a14fe6ac3da81959799354057
Flattened Threat <flattenedthreat@Shantis-MacBook-Air.local>
2016-04-03 21:31:44 -0700

    styling mapPin container

--- mobile/src/components/map/map-markers.js
+++ mobile/src/components/map/map-markers.js
@@ -1,6 +1,7 @@
 var React = require('react-native');
 
 var {
+  StyleSheet,
   TouchableOpacity,
   Text,
   Image
@@ -9,20 +10,50 @@ var {
 
 module.exports = {
 
+  selected: false,
+
   createPin: function( callback ) {
     return {
       longitude: -122.4046101306659,
       latitude: 37.78151983393839,
-      view: <TouchableOpacity style={ { alignItems: 'center' } } onPress={ callback } >
-        <Text style={{fontWeight: 'bold', color: '#f007'}}>
-          cooler spot!
-        </Text>
-        <Image
-          style={{width: 20, height: 20, resizeMode: 'cover'}}
-          source={ require('../images/icon_pub.png') }
-        />
-      </TouchableOpacity>
+      view:
+        <TouchableOpacity style={ styles.marker } onPress={ this.handleClick } >
+          { this.selected ? <Text style={ styles.title } >cooler spot!</Text> : null }
+          <Image style={ styles.image }  source={ require('../images/icon_pub.png') } />
+        </TouchableOpacity>
     }
+  },
+
+  handleClick: function() {
+    alert( "selected is:", this.selected );
+    if( this.selected ) {
+      callback();
+    }
+    this.selected = !this.selected;
   }
 
 };
+
+
+var styles = StyleSheet.create({
+
+  marker: {
+    alignItems: 'center',
+    padding: 3,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: 'blue',
+  },
+
+  title: {
+    fontWeight: 'bold',
+    color: 'green'
+  },
+
+  image: {
+    width: 20,
+    height: 20,
+    resizeMode: 'cover'
+  }
+
+});
