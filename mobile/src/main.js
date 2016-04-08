8a34c69f669feb009f510b4fb12550d4f975f693
Flattened Threat <flattenedthreat@Shantis-MacBook-Air.local>
2016-04-07 19:32:46 -0700

    working navbar

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -2,6 +2,7 @@ var React = require('react-native');
 var Map = require('./components/map/map');
 var Activity = require('./components/activities/activity');
 var Camera = require('./components/camera/camera');
+var NavBar = require('./components/common/navbar');
 
 var {
   Navigator,
@@ -19,12 +20,14 @@ module.exports = React.createClass({
   // every veiw gets access to navigator
   renderScene: function (route, navigator){
     var Component = Routes[ route.name ];
-    return <Component route={route} navigator={navigator} />;
+    return <Component route={route} navigator={ navigator } />;
   },
 
   render: function () {
     return (
       <Navigator
+        style={ { position: 'relative' } }
+        navigationBar={ <NavBar /> }
         initialRoute={ {name: 'map'} }
         renderScene={ this.renderScene }
         configureScene={ ()=> Navigator.SceneConfigs.FloatFromRight }
